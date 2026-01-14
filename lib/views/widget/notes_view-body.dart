dad3fe152de96ce7b554d3577cb45fce1d91c7ff
younesse <younesse.amzil@gmail.com>
2026-01-14 10:26:51 +0100

    Create listView Notes

--- lib/views/widget/notes_view-body.dart
+++ lib/views/widget/notes_view-body.dart
@@ -7,6 +7,28 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [SizedBox(height: 32), CustomAppBar(), NoteItem()]);
+    return Column(
+      children: [
+        SizedBox(height: 32),
+        CustomAppBar(),
+        Expanded(child: ListViewNotes()),
+      ],
+    );
+  }
+}
+
+class ListViewNotes extends StatelessWidget {
+  const ListViewNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: NoteItem(),
+        );
+      },
+    );
   }
 }
