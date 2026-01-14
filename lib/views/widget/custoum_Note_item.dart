6cb01fd6cf7d282dc5d8a830d7b19c10286f42e7
younesse <younesse.amzil@gmail.com>
2026-01-14 01:55:09 +0100

    Create custom Note Iteam

---
+++ lib/views/widget/custoum_Note_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 16, left: 14, bottom: 24),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Color(0xffFFcc80),
+      ),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Trips',
+              style: TextStyle(color: Colors.black, fontSize: 32),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 13),
+              child: Text(
+                'Build your Career whith younesse_Malek',
+                style: TextStyle(
+                  color: Colors.black.withAlpha(170),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.black, size: 27),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10, right: 29),
+            child: Text(
+              'May 21,2026',
+              style: TextStyle(color: Colors.black.withAlpha(188)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widget/notes_view-body.dart
+++ lib/views/widget/notes_view-body.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widget/coustom_app_bar.dart';
+import 'package:notes_app/views/widget/custoum_Note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [SizedBox(height: 32), CustomAppBar()]);
+    return Column(children: [SizedBox(height: 32), CustomAppBar(), NoteItem()]);
   }
 }
