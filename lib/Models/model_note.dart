8de4cbfdecc6e09dd3d5ddf36d29e58834c9be04
younesse <younesse.amzil@gmail.com>
2026-01-15 23:53:29 +0100

    create note model

---
+++ lib/Models/model_note.dart
@@ -0,0 +1,13 @@
+class ModelNote {
+  final String title;
+  final String content;
+  final String date;
+  final int color;
+
+  ModelNote({
+    required this.date,
+    required this.color,
+    required this.content,
+    required this.title,
+  });
+}
