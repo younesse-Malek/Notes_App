2d397641a03015481306ac1db3c119a229e3750d
younesse <younesse.amzil@gmail.com>
2026-01-16 07:17:42 +0100

    Init Hive and open box

--- lib/const/constance.dart
+++ lib/const/constance.dart
@@ -3,4 +3,4 @@ import 'package:flutter/cupertino.dart';
 const kPrimeryColor = Color(0xff62FCD7);
 const String notesViewId = 'notesViewId';
 const String editNoteID = 'editNoteID';
-
+const String kNotesBox = 'Note_Box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/const/constance.dart';
 import 'package:notes_app/views/edite_Notes.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
@@ -15,8 +19,8 @@ class NotesApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       routes: {
-         NotesView().id : (context)=>  NotesView(),
-         EditeNotes().editeNodeID :(context)=>EditeNotes(),
+        NotesView().id: (context) => NotesView(),
+        EditeNotes().editeNodeID: (context) => EditeNotes(),
       },
       initialRoute: const NotesView().id,
     );
